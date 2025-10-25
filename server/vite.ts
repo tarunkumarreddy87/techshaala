f2b426f3316878da2be7b3c161ab9960d7bd2723
tarunkumarreddy87 <tarunkumarreddyp487@gmail.com>
2025-10-25 21:10:06 +0530

    Fix syntax error in vite.ts: Remove extra parentheses in Error constructor

--- server/vite.ts
+++ server/vite.ts
@@ -73,7 +73,7 @@ export function serveStatic(app: Express) {
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
